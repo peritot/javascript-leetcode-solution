d9204f320a5d8ee08a19b3b6ff91ff18ce88a258
peritot <peritotchan@gmail.com>
2019-09-04 20:47:21 +0800

    Add mergeTwoLists

---
+++ solution/21.mergeTwoLists.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const mergeTwoLists = function (l1, l2) {
+  let head = new ListNode(0);
+
+  let h = head;
+  while (l1 !== null && l2 !== null) {
+    if (l1.val < l2.val) {
+      h.next = l1;
+      l1 = l1.next;
+    } else {
+      h.next = l2;
+      l2 = l2.next;
+    }
+    h = h.next;
+  }
+  h.next = l1 !== null ? l1 : l2;
+
+  return head.next;
+}
