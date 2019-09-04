8624be175c8bfc0f19f36f95bc97de364b685bc4
peritot <peritotchan@gmail.com>
2019-09-04 10:25:18 +0800

    Better isValid

--- solution/20.isValid.js
+++ solution/20.isValid.js
@@ -3,64 +3,23 @@
  * @return {boolean}
  */
 const isValid = function (s) {
-  let a = 0;
-  let b = 0;
-  let c = 0;
-  let ai = [];
-  let bi = [];
-  let ci = [];
-  let nums = [];
-  let index;
+  let map = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+  };
+
+  let stack = [];
   for (let i = 0; i < s.length; i++) {
-    switch (s[i]) {
-      case '(':
-        a++;
-        ai.push(i);
-        break;
-      case '[':
-        b++;
-        bi.push(i);
-        break;
-      case '{':
-        c++;
-        ci.push(i);
-        break;
-      case ')':
-        a--;
-        index = ai.pop();
-        if (index > 0 && (nums[index].b !== b || nums[index].c !== c)) {
-          return false;
-        }
-        break;
-      case ']':
-        b--;
-        index = bi.pop();
-        if (index > 0 && (nums[index].a !== a || nums[index].c !== c)) {
-          return false;
-        }
-        break;
-      case '}':
-        c--;
-        index = ci.pop();
-        if (index > 0 && (nums[index].a !== a || nums[index].b !== b)) {
-          return false;
-        }
-        break;
-      default:
-        break;
+    let c = s[i];
+    if (map[c]) {
+      if (stack.pop() !== map[c]) {
+        return false;
+      }
+    } else {
+      stack.push(c);
     }
-    if (a < 0 || b < 0 || c < 0) {
-      return false;
-    }
-    nums.push({
-      a,
-      b,
-      c
-    });
-  }
-  if (a !== 0 || b !== 0 || c !== 0) {
-    return false;
   }
 
-  return true;
+  return stack.length === 0;
 }
